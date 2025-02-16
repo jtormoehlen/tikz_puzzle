2bdba42f85c52aa0b6184622ac5355fec23e125d
Johannes Tormoehlen <jtormoehlen@uni-osnabrueck.de>
2025-02-16 10:06:58 +0100

    Update exercises

--- majority.py
+++ majority.py
@@ -1,11 +1,3 @@
-import random as rand
-
-def count_elem(arr, elem, left, right):
-    count = 0
-    for i in range(left, right + 1):
-        if arr[i] == elem:
-            count += 1
-    return count
 
 def maj_elem(arr, left, right):
 
@@ -16,17 +8,11 @@ def maj_elem(arr, left, right):
     maj_left = maj_elem(arr, left, mid)
     maj_right = maj_elem(arr, mid + 1, right)
 
-    print(arr[left:mid])
-    print(arr[mid+1:right])
-    print(f'left: {maj_left} | right: {maj_right}')
-
     if maj_left == maj_right:
         return maj_left
 
     count_left = sum(1 for i in range(left, right + 1) if arr[i] == maj_left)
     count_right = sum(1 for i in range(left, right + 1) if arr[i] == maj_right)
-    # count_left = count_elem(arr, maj_left, left, right)
-    # count_right = count_elem(arr, maj_right, left, right)
 
     if count_left > (right - left + 1) // 2:
         return maj_left
@@ -35,7 +21,7 @@ def maj_elem(arr, left, right):
     else:
         return None
 
-    # Example usage 
+# Example usage 
 # arr = [2, 2, 1, 1, 1, 2, 2] 
 # arr = [6, 7, 6, 6, 6, 3, 2, 6, 6, 1, 6]
 arr = [i for i in range(10)]
--- palindrome.py
+++ palindrome.py
@@ -16,7 +16,7 @@ def gernerate_palindrome(length=100):
     
     return palindrome
 
-def get_complement(seq):
+def get_compl(seq):
     # Generate the complement of a DNA sequence
     complement = ''
     for base in seq:
@@ -32,11 +32,9 @@ def get_complement(seq):
 
 def is_palindromic(seq):
     # Check if the given sequence is palindromic
-    return seq == get_complement(seq[::-1])
+    return seq == get_compl(seq[::-1])  # return seq == seq[::-1] # distractor
 
-def find_longest_palindromic_sequence(dna, start=0, end=None, longest_seq=''):
-    if end is None:
-        end = len(dna)
+def longest_palindrome(dna, start, end, longest_seq=''):
 
     # Base case: If the starting index exceeds the end of the sequence
     if start >= end:
@@ -44,24 +42,24 @@ def find_longest_palindromic_sequence(dna, start=0, end=None, longest_seq=''):
 
     # Check all possible subsequences (minimum length 2)
     for length in range(2, end - start + 1):
-        subseq = dna[start:start + length]
-        complement = get_complement(subseq)
+        subseq = dna[start:start + length]  # subseq = dna[start:length] # distractor
+        compl = get_compl(subseq)
 
-        if is_palindromic(subseq) or subseq == complement:
+        if is_palindromic(subseq) or subseq == compl:
             if len(subseq) > len(longest_seq):
                 longest_seq = subseq
 
     # Recursive call for the next starting index
-    return find_longest_palindromic_sequence(dna, start + 1, end, longest_seq)
+    return longest_palindrome(dna, start + 1, end, longest_seq)
 
 # Example DNA sequence
 # dna_sequence = "AGCTTAGCAGCTCGAATTCG"
 # dna_sequence = "GTAACTCATGAGTTAC"
 dna_sequence = "XCTAGXXCTAGX"
 
-_dna_sequence = get_complement(dna_sequence)
+_dna_sequence = get_compl(dna_sequence)
 print(dna_sequence)
 print(_dna_sequence)
 # dna_sequence = "XXCTAGXCTAGXX"
-palindromic_sequences = find_longest_palindromic_sequence(dna_sequence)
+palindromic_sequences = longest_palindrome(dna_sequence, 0, len(dna_sequence))
 print("Palindromische Sequenzen:", palindromic_sequences)
