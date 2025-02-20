ffb8545e6c367b6cd5ec0252199936475b464c48
Johannes Tormoehlen <jtormoehlen@uni-osnabrueck.de>
2025-02-20 12:13:00 +0100

    Update majority exercise

--- majority.py
+++ majority.py
@@ -1,18 +1,18 @@
 
-def maj_elem(arr, left, right):
+def maj_elem(numbers, left, right):
 
-    if left == right:
-        return arr[left]
+    if left == right:  # if left >= right: #distractor
+        return numbers[left]  # return None #distractor
 
     mid = (left + right) // 2
-    maj_left = maj_elem(arr, left, mid)
-    maj_right = maj_elem(arr, mid + 1, right)
+    maj_left = maj_elem(numbers, left, mid)  # maj_elem(numbers, left, mid) #distractor
+    maj_right = maj_elem(numbers, mid + 1, right)  # maj_elem(numbers, mid + 1, right) #distractor
 
-    if maj_left == maj_right:
-        return maj_left
+    if maj_left == maj_right:  # if maj_left != maj_right: #distractor
+        return maj_left  # return None #distractor
 
-    count_left = sum(1 for i in range(left, right + 1) if arr[i] == maj_left)
-    count_right = sum(1 for i in range(left, right + 1) if arr[i] == maj_right)
+    count_left = sum(1 for i in range(left, right + 1) if numbers[i] == maj_left)
+    count_right = sum(1 for i in range(left, right + 1) if numbers[i] == maj_right)
 
     if count_left > (right - left + 1) // 2:
         return maj_left
